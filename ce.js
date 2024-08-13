b7a8abd51b18f7e6cdf0234f833e0bd89e0e3a56
joshua-sc <joshua-sc@wix.com>
2024-08-13 10:27:02 +0300

    more

--- ce.js
+++ ce.js
@@ -1,3 +1,7 @@
+import { createClient } from "@wix/sdk";
+import { site } from "@wix/site";
+import { bookings } from "@wix/site-bookings";
+
 class ColoredBox extends HTMLElement {
   constructor() {
     super();
@@ -29,19 +33,57 @@ class ColoredBox extends HTMLElement {
     if (name === 'color' && oldValue !== newValue) {
       this.setColor(newValue);
     } else if (name === 'bookingsServiceId' && oldValue !== newValue) {
-      this.setTextContent(newValue);
-    }
-  }
+      wixClient.products.queryProducts().find().then((products) => {
+        console.log(`Custom Element loaded Wix site Products: ${products.items}`)
+      });
 
-  // Set the color of the box
-  setColor(color) {
-    this.container.style.backgroundColor = color || 'gray';
-  }
+      let startRange = new Date();
+      let endRange = new Date();
+      endRange.setDate(startRange.getDate() + 365); // one year from now
 
-  // Set the text content of the box
-  setTextContent(text) {
-    this.textElement.textContent = text || '';
+      // get the service availability for the upcoming year
+      const rangeOptions = {
+        startDateTime: startRange,
+        endDateTime: endRange,
+      };
+      const availability = await bookings.getServiceAvailability(
+        serviceId,
+        rangeOptions,
+      );
+      const slots = availability.slots;
+
+      // get the date of the first available slot
+      const options = { weekday: "long", day: "numeric", month: "short" };
+      const firstSlotDate = slots[0].startDateTime.toLocaleDateString(
+        "en-US",
+        options,
+      );
+
+      this.setTextContent("Next availability: " + firstSlotDate);
+    };
+
+
   }
+}
+
+accessTokenListener(accessTokenGetter) {
+  const wixClient = createClient({
+    host: site.host({ applicationId: "766caf23-967f-4b3f-9999-8ece1efdf29b" }),
+    auth: site.auth(accessTokenGetter),
+    modules: { products },
+  });
+  console.log(`createClient`, wixClient)
+
+}
+
+// Set the color of the box
+setColor(color) {
+  this.container.style.backgroundColor = color || 'gray';
+}
+
+// Set the text content of the box
+setTextContent(text) {
+  this.textElement.textContent = text || '';
 }
 
 customElements.define('ce-josh', ColoredBox);
